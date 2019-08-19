7fbd4307c8b10ef5a38df5d74662e99952055f13
julien93140 <julien.moratalla@gmail.com>
2019-08-19 15:25:06 +0200

    goals in process

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals([...courseGoals, enteredGoal])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello this is my first react native app !</Text>
+    <View style={styles.screen}>
+      <View style={styles.content}>
+        <TextInput 
+          placeholder="Add a goal" 
+          style={styles.input} 
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+
+      <View style={styles.input}>
+
+      </View>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  content: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center'
   },
+  screen: {
+    padding: 50
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black', 
+    borderBottomWidth: 1, 
+    padding: 10 
+  }
 });
